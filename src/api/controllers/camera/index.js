3b4823c369a436978f40153b991d1da980c2696a
TuanAnh1003 <anhaanh2003@gmail.com>
2023-10-14 00:28:00 +0700

    fix: wrong import path in router

---
+++ src/api/controllers/camera/index.js
@@ -0,0 +1,5 @@
+export { default as createCamera } from './createCamera.js'
+export { default as deleteCameraById } from './deleteCameraById.js'
+export { default as getCameraById } from './getCameraById.js'
+export { default as updateCameraById } from './updateCameraById.js'
+export { default as getAllCameras } from './getAllCameras.js'
