bd67e4c33295f8274c20744b67870311893f46b8
TuanAnh1003 <anhaanh2003@gmail.com>
2023-10-29 18:17:46 +0700

    fix: update name folder in cloud

--- src/api/middlewares/upload.middleware.js
+++ src/api/middlewares/upload.middleware.js
@@ -1,17 +1,21 @@
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../../config/cloudinary.config.js";
-const today = new Date();
-const year = today.getFullYear().toString();
-const month = (today.getMonth() + 1).toString().padStart(2, "0");
-const day = today.getDate().toString().padStart(2, "0");
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../../config/cloudinary.config.js';
+
+const getFolderName = () => {
+  const today = new Date();
+  const year = today.getFullYear().toString();
+  const month = (today.getMonth() + 1).toString().padStart(2, '0');
+  const day = today.getDate().toString().padStart(2, '0');
+  return `${day}-${month}-${year}`;
+};
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: `${day}/${month}/${year}`,
-    public_id: (req, file) => file.originalname,
-  },
+    folder: getFolderName(),
+    public_id: (req, file) => file.originalname
+  }
 });
 
 const upload = multer({ storage });
