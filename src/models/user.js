46e3deefc1c96327ac16d2ffcc0e830935cf321a
TuanAnh1003 <anhaanh2003@gmail.com>
2023-12-13 22:42:54 +0700

    fix: update model using utf8

--- src/models/user.js
+++ src/models/user.js
@@ -3,20 +3,34 @@ import sequelize from '../config/database.config.js';
 
 const User = sequelize.define('user', {
   id: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(50),
     primaryKey: true,
   },
   username: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(50), // Set a specific length, adjust as needed
     allowNull: false,
     unique: true,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
   },
   email: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(50), // Set a specific length, adjust as needed
     allowNull: false,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
   },
   password: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(200), // Set a specific length, adjust as needed
+    allowNull: false,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
     allowNull: false,
   },
 });
--- src/models/vehicle.js
+++ src/models/vehicle.js
@@ -4,40 +4,55 @@ import Owner from "./owner.js";
 
 const Vehicle = sequelize.define("vehicle", {
   id: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(30),
     primaryKey: true,
   },
   vehicleName: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(50),
     allowNull: false,
+    charset: 'utf8mb4', // Thêm hỗ trợ UTF-8
+    collate: 'utf8mb4_unicode_ci', // Thêm hỗ trợ kiểu ký tự
   },
   licensePlate: {
-    type: Sequelize.STRING,
-    unique: true,
+    type: Sequelize.STRING(20),
+    unique: true
   },
-  ownerID: Sequelize.STRING,
+  ownerID: {
+    type: Sequelize.STRING(30),
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
+  },
   vehicleType: Sequelize.ENUM("car", "motorcycle"),
   engineCapacity: Sequelize.FLOAT,
   color: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(20),
     allowNull: false,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
   },
   frameNumber: {
-    type: Sequelize.STRING,
-    allowNull: false,
+    type: Sequelize.STRING(20),
+    allowNull: false
   },
   engineNumber: {
-    type: Sequelize.STRING,
-    allowNull: false,
+    type: Sequelize.STRING(20),
+    allowNull: false
   },
   brand: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(20),
     allowNull: false,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
   },
   imageUrl: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(50),
     allowNull: false,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
   },
+}, {
+  charset: 'utf8mb4',
+  collate: 'utf8mb4_unicode_ci',
 });
 
 Vehicle.belongsTo(Owner, { foreignKey: "ownerID", onDelete: "CASCADE" });
